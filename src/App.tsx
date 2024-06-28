09357e32e7d35d54f856157a09f1910e2e3ffabc
Ubuntu <howardzchung@gmail.com>
2024-06-28 21:47:42 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+try {
+  const wakeLock = await navigator.wakeLock.request("screen");
+  console.log(wakeLock);
+} catch (err: any) {
+  // the wake lock request fails - usually system related, such being low on battery
+  console.log(`${err.name}, ${err.message}`);
+}
+
+function App() {
+  const [pass, setPass] = useState(false)
+  return (
+      <div className={`main ${pass ? 'pass' : 'active'}`} onClick={() => setPass(!pass)}>
+        {pass ? 'PASS' : 'ACTIVE'}
+      </div>
+  )
+}
+
+export default App;
