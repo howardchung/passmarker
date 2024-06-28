894c12c8fa6810ecea9740bfb951e1d2294beb0d
Ubuntu <howardzchung@gmail.com>
2024-06-28 21:51:32 +0000

    wrap async

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
-try {
-  const wakeLock = await navigator.wakeLock.request("screen");
-  console.log(wakeLock);
-} catch (err: any) {
-  // the wake lock request fails - usually system related, such being low on battery
-  console.log(`${err.name}, ${err.message}`);
+async function getWakeLock() {
+  try {
+    const wakeLock = await navigator.wakeLock.request("screen");
+    console.log(wakeLock);
+  } catch (err: any) {
+    // the wake lock request fails - usually system related, such being low on battery
+    console.log(`${err.name}, ${err.message}`);
+  }
 }
+getWakeLock();
 
 function App() {
   const [pass, setPass] = useState(false)
